e9a079f8d0deccd138694f7bb65e2c0919d7e1ec
doRnsns98 <61038091+Kimyerim1935@users.noreply.github.com>
2023-05-11 10:32:18 +0900

    init: project settings

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,5 @@
-/** @type { import('@storybook/react-webpack5').StorybookConfig } */
-const config = {
+import type { StorybookConfig } from "@storybook/react-webpack5";
+const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
--- src/stories/Page.stories.ts
+++ src/stories/Page.stories.ts
@@ -1,20 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react';
 import { within, userEvent } from '@storybook/testing-library';
 
 import { Page } from './Page';
 
-export default {
+const meta = {
   title: 'Example/Page',
   component: Page,
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'fullscreen',
   },
-};
+} satisfies Meta<typeof Page>;
 
-export const LoggedOut = {};
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const LoggedOut: Story = {};
 
 // More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
-export const LoggedIn = {
+export const LoggedIn: Story = {
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
     const loginButton = await canvas.getByRole('button', {
