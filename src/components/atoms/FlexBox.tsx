a9fd73d2a56ef38c9ccfb25cf9e097942e8d0325
김가영 <namiein@naver.com>
2023-05-13 14:01:06 +0900

    feat : FlexBox 컴포넌트 추가

---
+++ src/components/atoms/FlexBox.tsx
@@ -0,0 +1,34 @@
+/** @jsxImportSource @emotion/react */
+
+import { css } from '@emotion/react';
+import { CSSInterpolation } from '@emotion/serialize';
+
+type FlexBoxType = {
+    children: React.ReactNode;
+    justifyContent?: string;
+    alignItems?: string;
+    direction?: 'row' | 'row-reverse' | 'column' | 'column-reverse';
+    gap?: number;
+    flex?: string;
+    customCSS?: CSSInterpolation;
+};
+
+const flexBoxStyle = css({
+    display: 'flex'
+});
+
+export default function FlexBox({
+    children,
+    justifyContent = 'unset',
+    alignItems = 'unset',
+    direction = 'row',
+    gap = 0,
+    flex = 'unset',
+    customCSS = {}
+}: FlexBoxType) {
+    return (
+        <div css={css([flexBoxStyle, { justifyContent, alignItems, flexDirection: direction, gap, flex }, customCSS])}>
+            {children}
+        </div>
+    );
+}
