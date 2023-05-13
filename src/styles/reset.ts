151e9b99f3eec8bb6f7d50eae01bc54d567f6df5
김가영 <namiein@naver.com>
2023-05-13 13:15:42 +0900

    feat: reset css 추가

--- src/styles/reset.ts
+++ src/styles/reset.ts
@@ -5,16 +5,58 @@ const reset = css`
         box-sizing: border-box;
     }
 
+    html {
+        width: 100%;
+    }
+
     body {
         margin: 0;
+        padding: 0;
         font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell',
             'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
+        font-size: 14px;
+        line-height: 1.5;
+        color: #000000;
+        background-color: #fafafa;
     }
 
     code {
         font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
+    }
+
+    input {
+        outline: none;
+    }
+
+    input:-webkit-autofill {
+        transition-delay: 9999s;
+        transition-property: background-color, color;
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    button {
+        border: 0;
+        outline: 0;
+        background-color: transparent;
+    }
+
+    button:disabled {
+        pointer-events: none;
+    }
+
+    ul,
+    menu {
+        list-style-type: none;
+    }
+
+    p,
+    span {
+        word-break: break-all;
     }
 `;
 
