f0161beae7b37af469c01e62d9c95a8a1bd43345
김예림 <61038091+Kimyerim1935@users.noreply.github.com>
2023-05-20 00:28:10 +0900

    add: color palette 추가

---
+++ src/styles/palette.ts
@@ -0,0 +1,15 @@
+const palette = {
+    primary: {
+        main: '#145CB1'
+    },
+    secondary: {
+        main: '#032A85'
+    },
+    disabled: {
+        main: '#85A3BD'
+    },
+    lightBlue: {
+        main: '#8BBCE4'
+    }
+};
+export default palette;
