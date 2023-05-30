e0f559881e4b4dd4bc4d66e8601d60ebddad7090
Gayoung Kim <52883505+akffkdahffkdgo77@users.noreply.github.com>
2023-05-30 15:26:15 +0900

    feat: TableBody Storybook

---
+++ src/components/atoms/TableBody.stories.tsx
@@ -0,0 +1,43 @@
+/** @jsxImportSource @emotion/react */
+import TableBody from 'components/atoms/TableBody';
+import TableCell from 'components/atoms/TableCell';
+import TableRow from 'components/atoms/TableRow';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+    title: 'ATOMS/TableBody',
+    component: TableBody,
+    tags: ['autodocs'],
+    argTypes: {
+        children: {
+            type: { name: 'string', required: true },
+            control: false,
+            description: '컴포넌트',
+            table: { category: 'required', type: { summary: 'React.ReactNode' } }
+        },
+        customCSS: {
+            control: { type: 'object' },
+            description: 'Custom CSS',
+            table: { category: 'style', defaultValue: { summary: '{}' }, type: { summary: 'CSSInterpolation' } }
+        }
+    }
+} satisfies Meta<typeof TableBody>;
+
+export default meta;
+type Story = StoryObj<typeof TableBody>;
+
+export const Default: Story = {
+    args: {
+        children: (
+            <TableRow>
+                <TableCell>1</TableCell>
+                <TableCell>바나나</TableCell>
+                <TableCell>바나나차차</TableCell>
+                <TableCell>1010101010</TableCell>
+                <TableCell>바나나나나나나나나나</TableCell>
+            </TableRow>
+        ),
+        customCSS: {}
+    }
+};
