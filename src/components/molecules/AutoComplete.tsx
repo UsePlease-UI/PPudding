e31a6ebb92ff221092bdd576d0841ef27758913d
doRnsns98 <61038091+Kimyerim1935@users.noreply.github.com>
2023-06-07 11:18:17 +0900

    mod: 수정사항 반영

--- src/components/molecules/AutoComplete.tsx
+++ src/components/molecules/AutoComplete.tsx
@@ -1,21 +1,121 @@
 /** @jsxImportSource @emotion/react */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 
-import ComboBox from 'components/molecules/ComboBox';
-import { AUTOCOMPLETE } from 'constants/autocomplete';
+import React, { useRef, useEffect } from 'react';
+
+import _ from 'lodash';
+
+import FlexBox from 'components/atoms/FlexBox';
 
 import { css } from '@emotion/react';
-import { ChevronDownIcon } from 'assets/icons';
 
-type autoCompleteType = {
+type ListType = {
+    idx: number;
     label: string;
+    value: string;
 };
 
-export default function AutoComplete({ label }: autoCompleteType) {
+type autoCompleteType = {
+    inputValue: string;
+    label: string;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    listArr: ListType[];
+};
+
+const listItemStyle = css({
+    width: '100%',
+    minWidth: 120,
+    fontSize: 14,
+    fontWeight: 400,
+    lineHeight: 1.5,
+    padding: '0 12px',
+    '& button': {
+        width: '100%',
+        height: 40,
+        textAlign: 'left',
+        textOverflow: 'ellipsis',
+        overflow: 'hidden',
+        whiteSpace: 'nowrap'
+    },
+    '&:hover, &:hover button': {
+        fontWeight: 600,
+        color: '#ffffff',
+        backgroundColor: 'lightpink'
+    }
+});
+
+const ulStyle = css({
+    maxWidth: 200,
+    display: 'block',
+    background: 'white',
+    borderRadius: 4,
+    border: '1px solid #e0e0e0',
+    boxSizing: 'border-box',
+    padding: '10px 0'
+});
+
+export default function AutoComplete({ inputValue, onChange, label, listArr }: autoCompleteType) {
+    const listRef = useRef<HTMLUListElement | null>(null);
+
+    useEffect(() => {
+        if (listArr.length) {
+            document.body.style.overflow = 'hidden';
+        } else document.body.style.overflow = 'auto';
+    }, [listArr]);
+
     return (
         <div>
             <label htmlFor={label} css={css({ display: 'none' })} />
-            <ComboBox id={label} icon={<ChevronDownIcon />} list={AUTOCOMPLETE} />
+            <FlexBox
+                justifyContent="center"
+                alignItems="center"
+                direction="row"
+                gap={5}
+                customCSS={{
+                    maxWidth: 200,
+                    height: 40,
+                    borderRadius: 4,
+                    border: '1px solid #eeeeee',
+                    background: 'white',
+                    boxSizing: 'border-box',
+                    padding: 10
+                }}
+            >
+                <input
+                    id={label}
+                    type="text"
+                    role="combobox"
+                    aria-autocomplete="list"
+                    aria-expanded="true"
+                    aria-controls="listbox"
+                    css={css({
+                        height: '100%',
+                        border: 0
+                    })}
+                    value={inputValue}
+                    onChange={onChange}
+                />
+            </FlexBox>
+            {Boolean(listArr.length > 0) && (
+                <ul ref={listRef} id="listbox" role="listbox" aria-label="States" css={ulStyle}>
+                    {listArr.map((el) => {
+                        return (
+                            <li key={`list-${el.idx}`} css={listItemStyle}>
+                                <button type="button">
+                                    {[...el.label].map((letter, idx) =>
+                                        inputValue.includes(letter) ? (
+                                            <span css={css({ color: 'hotPink', fontWeight: 700 })}>{letter}</span>
+                                        ) : (
+                                            // eslint-disable-next-line react/no-array-index-key
+                                            <React.Fragment key={`letter-${idx}`}>{letter}</React.Fragment>
+                                        )
+                                    )}
+                                </button>
+                            </li>
+                        );
+                    })}
+                </ul>
+            )}
         </div>
     );
 }
--- src/examples/AutoCompleteExample.tsx
+++ src/examples/AutoCompleteExample.tsx
@@ -1,11 +1,52 @@
+import { useState, useCallback } from 'react';
+
+import _ from 'lodash';
+
 import FlexBox from 'components/atoms/FlexBox';
 import AutoComplete from 'components/molecules/AutoComplete';
+import { AUTOCOMPLETE } from 'constants/autocomplete';
+
+type ListType = {
+    idx: number;
+    label: string;
+    value: string;
+};
 
 export default function AutoCompleteExample() {
+    const list = AUTOCOMPLETE;
+    const [listArr, setListArr] = useState<ListType[]>(list);
+    const [isVisible, setIsVisible] = useState<boolean>(false);
+    const [inputValue, setInputValue] = useState<string>('');
+
+    const handleSearch = useCallback(
+        _.debounce((value: string) => {
+            let newArr = [];
+            if (value.length !== 0 || value !== '') {
+                setIsVisible(true);
+
+                newArr = list.filter((el: ListType) => el.label.includes(value));
+                setListArr(newArr);
+            } else {
+                setIsVisible(false);
+            }
+
+            if (value.length === 0) {
+                setListArr(list);
+            }
+        }, 100),
+        []
+    );
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { value } = e.target;
+        setInputValue(value);
+        handleSearch(value);
+    };
+
     return (
         <FlexBox direction="column" gap={10}>
             <h2>AutoComplete</h2>
-            <AutoComplete label="autoComplete" />
+            <AutoComplete label="autoComplete" listArr={listArr} inputValue={inputValue} onChange={handleChange} />
         </FlexBox>
     );
 }
