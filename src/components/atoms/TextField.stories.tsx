048c9a455593cc8655253c00d57f1205b533917b
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2023-06-07 15:09:03 +0900

    mod : TextField Storybook doc 업데이트

--- src/components/atoms/TextField.stories.tsx
+++ src/components/atoms/TextField.stories.tsx
@@ -5,84 +5,137 @@ import TextField from 'components/atoms/TextField';
 import type { Meta, StoryObj } from '@storybook/react';
 
 const meta = {
-    title: 'ATOMS/TextField',
+    title: 'Form/TextField',
     component: TextField,
     tags: ['autodocs'],
     argTypes: {
         name: {
+            type: { name: 'function', required: true },
             control: { type: 'text' },
             description: 'TextField 이름',
-            table: { category: 'required', type: { summary: 'string' } }
+            table: {
+                category: 'required',
+                type: { summary: 'string' }
+            }
         },
         value: {
+            type: { name: 'function', required: true },
             control: false,
             description: 'TextField 값',
-            table: { category: 'required', type: { summary: 'string' } }
+            table: {
+                category: 'required',
+                type: { summary: 'string' }
+            }
         },
         onChange: {
+            type: { name: 'function', required: true },
             control: false,
             description: 'Change Event Handler',
-            table: { category: 'required', type: { summary: 'function' } }
+            table: {
+                category: 'required',
+                type: {
+                    summary: '(e: React.ChangeEvent<HTMLInputElement>) => void'
+                }
+            }
         },
         isDisabled: {
             control: { type: 'boolean' },
             description: '활성화여부',
-            table: { category: 'States', defaultValue: { summary: false }, type: { summary: 'boolean' } }
+            table: {
+                category: 'optional',
+                defaultValue: { summary: false },
+                type: { summary: 'boolean' }
+            }
         },
         isReadOnly: {
             control: { type: 'boolean' },
             description: '읽기 전용',
-            table: { category: 'States', defaultValue: { summary: false }, type: { summary: 'boolean' } }
+            table: {
+                category: 'optional',
+                defaultValue: { summary: false },
+                type: { summary: 'boolean' }
+            }
         },
         isFullWidth: {
             control: { type: 'boolean' },
             description: '[CSS] width (100% 또는 기본 값)',
-            table: { category: 'States', defaultValue: { summary: false }, type: { summary: 'boolean' } }
-        },
-        title: {
-            control: { type: 'text' },
-            description: '[웹 접근성] Title 속성',
-            table: { category: 'Accessibility', type: { summary: 'string' } }
+            table: {
+                category: 'optional',
+                defaultValue: { summary: false },
+                type: { summary: 'boolean' }
+            }
         },
         type: {
             control: { type: 'inline-radio' },
             description: '[속성] TextField 타입',
             options: ['text', 'number', 'url', 'email', 'password', 'phone'],
             table: {
-                category: 'Input Attributes',
+                category: 'optional',
                 defaultValue: { summary: 'text' },
-                type: { summary: 'text | number | url | email | password | phone' }
+                type: {
+                    summary: 'text | number | url | email | password | phone'
+                }
             }
         },
         placeholder: {
             control: { type: 'text' },
             description: '[속성] TextField 안내문',
-            table: { category: 'Input Attributes', defaultValue: { summary: '""' }, type: { summary: 'string' } }
+            table: {
+                category: 'optional',
+                defaultValue: { summary: '""' },
+                type: { summary: 'string' }
+            }
         },
         maxLength: {
             control: { type: 'number' },
             description: '[속성] TextField 값 제한',
-            table: { category: 'Input Attributes', defaultValue: { summary: 100 }, type: { summary: 'number' } }
+            table: {
+                category: 'optional',
+                defaultValue: { summary: 100 },
+                type: { summary: 'number' }
+            }
         },
         onKeyDown: {
             control: false,
             description: 'Key Event Handler',
-            table: { category: 'optional', defaultValue: { summary: '() => {}' }, type: { summary: 'function' } }
+            table: {
+                category: 'optional',
+                defaultValue: { summary: '() => {}' },
+                type: {
+                    summary: '(e: React.KeyboardEvent<HTMLInputElement>) => void'
+                }
+            }
         },
         onFocus: {
             control: false,
             description: 'Focus Event Handler',
-            table: { category: 'optional', defaultValue: { summary: '() => {}' }, type: { summary: 'function' } }
+            table: {
+                category: 'optional',
+                defaultValue: { summary: '() => {}' },
+                type: {
+                    summary: '(e: React.FocusEvent<HTMLInputElement>) => void'
+                }
+            }
         },
         onBlur: {
             control: false,
             description: 'Blur Event Handler',
-            table: { category: 'optional', defaultValue: { summary: '() => {}' }, type: { summary: 'function' } }
+            table: {
+                category: 'optional',
+                defaultValue: { summary: '() => {}' },
+                type: {
+                    summary: '(e: React.FocusEvent<HTMLInputElement>) => void'
+                }
+            }
         },
         customCSS: {
             control: { type: 'object' },
             description: 'Custom CSS',
-            table: { category: 'Style', defaultValue: { summary: '{}' }, type: { summary: 'CSSInterpolation' } }
+            table: {
+                category: 'Style',
+                defaultValue: { summary: '{}' },
+                type: { summary: 'CSSInterpolation' }
+            }
         }
     }
 } satisfies Meta<typeof TextField>;
@@ -100,7 +153,6 @@ export const Default: Story = {
         isReadOnly: false,
         isDisabled: false,
         isFullWidth: true,
-        title: '이름',
         type: 'text',
         placeholder: '이름을 입력해주세요.',
         maxLength: 100,
