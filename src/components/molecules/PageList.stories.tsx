c5eac41aa015f5a43f8b764ec1315c3daec55955
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2023-06-07 15:21:29 +0900

    Update PageList.stories.tsx

--- src/components/molecules/PageList.stories.tsx
+++ src/components/molecules/PageList.stories.tsx
@@ -5,64 +5,88 @@ import PageList from 'components/molecules/PageList';
 import type { Meta, StoryObj } from '@storybook/react';
 
 const meta = {
-    title: 'MOLECULES/PageList',
+    title: 'Pagination/PageList',
     component: PageList,
     tags: ['autodocs'],
     argTypes: {
         totalCount: {
+            type: { name: 'string', required: true },
             control: { type: 'number' },
             description: '전체 페이지 버튼 개수',
-            table: { category: 'REQUIRED' }
+            table: { category: 'required' }
         },
         page: {
+            type: { name: 'string', required: true },
             control: false,
             description: '현재 선택된 페이지',
-            table: { category: 'REQUIRED' }
+            table: { category: 'required' }
         },
         pageLimit: {
+            type: { name: 'string', required: true },
             control: { type: 'number' },
             description: '한 번에 보여줄 페이지의 최댓값',
-            table: { category: 'REQUIRED' }
+            table: { category: 'required' }
         },
         blockNum: {
+            type: { name: 'string', required: true },
             control: false,
             description: '페이지 순서 파악',
-            table: { category: 'REQUIRED' }
+            table: { category: 'required' }
         },
         onChange: {
+            type: { name: 'string', required: true },
             control: false,
             description: '페이지 이동',
-            table: { category: 'REQUIRED', type: { summary: 'function' } }
+            table: {
+                category: 'required',
+                type: { summary: '(page: number, newBlockNum: number) => void' }
+            }
         },
         handleFirstClick: {
+            type: { name: 'string', required: true },
             control: false,
             description: '맨 첫 페이지 이동',
-            table: { category: 'REQUIRED', type: { summary: 'function' } }
+            table: {
+                category: 'required',
+                type: { summary: '() => void' }
+            }
         },
         handlePrevClick: {
+            type: { name: 'string', required: true },
             control: false,
             description: '이전 페이지 이동',
-            table: { category: 'REQUIRED', type: { summary: 'function' } }
+            table: {
+                category: 'required',
+                type: { summary: '() => void' }
+            }
         },
         handleNextClick: {
+            type: { name: 'string', required: true },
             control: false,
             description: '다음 페이지 이동',
-            table: { category: 'REQUIRED', type: { summary: 'function' } }
+            table: {
+                category: 'required',
+                type: { summary: '() => void' }
+            }
         },
         handleLastClick: {
+            type: { name: 'string', required: true },
             control: false,
             description: '맨 마지막 페이지 이동',
-            table: { category: 'REQUIRED', type: { summary: 'function' } }
+            table: {
+                category: 'required',
+                type: { summary: '() => void' }
+            }
         },
         showFirstButton: {
             control: { type: 'boolean' },
             description: '맨 첫 페이지로 이동하는 버튼 노출 여부',
-            table: { category: 'OPTIONAL' }
+            table: { category: 'optional' }
         },
         showLastButton: {
             control: { type: 'boolean' },
             description: '맨 마지막 페이지로 이동하는 버튼 노출 여부',
-            table: { category: 'OPTIONAL' }
+            table: { category: 'optional' }
         }
     }
 } satisfies Meta<typeof PageList>;
@@ -93,14 +117,14 @@ const PaginationControlTemplate: Story = {
     }
 };
 
-export const PaginationListControl: Story = {
+export const Default: Story = {
     ...PaginationControlTemplate,
     args: {
-        showFirstButton: true,
-        showLastButton: true,
-        totalCount: 10,
-        pageLimit: 10,
         page: 1,
-        blockNum: 1
+        blockNum: 1,
+        pageLimit: 10,
+        totalCount: 10,
+        showFirstButton: true,
+        showLastButton: true
     }
 };
