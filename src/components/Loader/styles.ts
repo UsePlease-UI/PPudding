7c69dd8a2ebfb8c4f6db1f73add2fb1a8b5d609e
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2024-01-08 12:33:17 +0900

    mod : loader style 분리

---
+++ src/components/Loader/styles.ts
@@ -0,0 +1,25 @@
+import { css, keyframes } from '@emotion/react';
+import { palette } from 'styles';
+
+const blink = keyframes`
+    0% {
+        opacity: 0.4;
+    }
+    50%{
+        opacity: 1;
+    }
+    100% {
+        opacity: 0.4;
+    }
+`;
+
+export const loaderStyle = {
+    skeleton: css({
+        cursor: 'progress',
+        width: '100%',
+        height: 30,
+        borderRadius: 4,
+        backgroundColor: palette.gray['100'],
+        animation: `${blink} 2s ease-in-out infinite`
+    })
+};
