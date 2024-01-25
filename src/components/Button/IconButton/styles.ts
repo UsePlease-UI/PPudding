56b17cc85674375bad74b6865a9f27da1e214452
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2024-01-25 21:52:33 +0900

    chore : 주석 제거

--- src/components/Button/IconButton/styles.ts
+++ src/components/Button/IconButton/styles.ts
@@ -9,45 +9,4 @@ export const iconButtonStyle = {
             display: 'block'
         }
     })
-    // mini: css({
-    //     padding: 6,
-    //     '& svg': {
-    //         width: 16,
-    //         height: 16
-    //     }
-    // }),
-    // small: css({
-    //     padding: 6.5,
-    //     '& svg': {
-    //         width: 20,
-    //         height: 20
-    //     }
-    // }),
-    // medium: css({
-    //     padding: 8,
-    //     '& svg': {
-    //         width: 24,
-    //         height: 24
-    //     }
-    // }),
-    // large: css({
-    //     padding: 8,
-    //     '& svg': {
-    //         width: 28,
-    //         height: 28
-    //     }
-    // })
 };
-
-// export function getSizeStyle(size?: IconButtonSizeType) {
-//     switch (size) {
-//         case 'large':
-//             return iconButtonStyle.large;
-//         case 'medium':
-//             return iconButtonStyle.medium;
-//         case 'small':
-//             return iconButtonStyle.small;
-//         default:
-//             return iconButtonStyle.mini;
-//     }
-// }
