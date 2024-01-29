e141d1301a9772f1c48b7464bf7dbe4845e5b8ff
김예림 <61038091+Kimyerim1935@users.noreply.github.com>
2024-01-29 23:36:13 +0900

    add: label option 추가

--- src/pages/Home/examples/constants.ts
+++ src/pages/Home/examples/constants.ts
@@ -62,7 +62,28 @@ export const CALENDER = [
         value: 'yearly'
     }
 ];
-
+export const CALENDER_LABEL_COLOR = [
+    {
+        idx: 1,
+        label: '빨강',
+        value: 'red'
+    },
+    {
+        idx: 2,
+        label: '주황',
+        value: 'orange'
+    },
+    {
+        idx: 3,
+        label: '노랑',
+        value: 'yellow'
+    },
+    {
+        idx: 4,
+        label: '초록',
+        value: 'green'
+    }
+];
 export const DRAG_N_DROP = [
     {
         idx: 1,
