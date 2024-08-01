022e0bddfbd7dc3e92aaff6ddc8121571ea99fff
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2024-08-01 19:32:54 +0900

    fix : build error

--- src/stories/components/Pagination/Pagination.stories.tsx
+++ src/stories/components/Pagination/Pagination.stories.tsx
@@ -17,25 +17,14 @@ type Story = StoryObj<typeof Pagination>;
 const PaginationControlTemplate: Story = {
     render: (args) => {
         const [value, setValue] = useState(1);
-        const [blockValue, setBlockValue] = useState(1);
-
-        return (
-            <Pagination
-                {...args}
-                page={value}
-                blockNum={blockValue}
-                onChange={(page: number, blockNum: number) => {
-                    setValue(page);
-                    setBlockValue(blockNum);
-                }}
-            />
-        );
+        return <Pagination {...args} page={value} onChange={setValue} />;
     }
 };
 
 export const Default: Story = {
     ...PaginationControlTemplate,
     args: {
-        totalCount: 10
+        totalCount: 10,
+        pageLimit: 5
     }
 };
