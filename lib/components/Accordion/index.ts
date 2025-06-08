276ada98d69ebf030cfa58cac25ee2e84850699a
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2025-06-08 14:11:15 +0900

    [I-114] Accordion Documentation (#115)

--- lib/components/Accordion/index.ts
+++ lib/components/Accordion/index.ts
@@ -2,7 +2,13 @@ import Accordion from './Accordion';
 import AccordionHeader from './AccordionHeader';
 import AccordionPanel from './AccordionPanel';
 
+// Component export
 export default Object.assign(Accordion, {
   Header: AccordionHeader,
   Panel: AccordionPanel,
 });
+
+// Types export
+export * from './Accordion';
+export * from './AccordionHeader';
+export * from './AccordionPanel';
