e8cb0d24f063d9596a1487645453c318e961d19d
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2025-06-25 20:16:23 +0900

    [I-56] Switch Component

--- lib/components/Form/index.ts
+++ lib/components/Form/index.ts
@@ -4,6 +4,9 @@ export * from './Checkbox';
 export { default as Radio } from './Radio';
 export * from './Radio';
 
+export { default as Switch } from './Switch';
+export * from './Switch';
+
 export { default as Textarea } from './Textarea';
 export * from './Textarea';
 
